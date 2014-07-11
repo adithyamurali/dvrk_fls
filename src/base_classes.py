a24a2764a623762f106cf1d03543757fee978f2c
adithyamurali <adithya_murali@berkeley.edu>
2014-07-11 16:04:51 -0700

    State Machine is running, yet to populate the execute methods

--- src/base_classes.py
+++ src/base_classes.py
@@ -4,11 +4,12 @@
 # UC Berkeley, 2014
 
 import time
-
+import tfx
 import smach
 
-SLEEP_TIME = 0
+from davinci_trajectory.raven_controller import RavenController
 
+SLEEP_TIME = 0
 
 class StateTestClass(smach.State):
     """ This is a the base test counterlass for the testing states.
@@ -19,8 +20,15 @@ class StateTestClass(smach.State):
     def __init__(self):
         self.counter = -1
         self.outcomes = None
+        singlePose =tfx.pose([0.02068052528579386, 0.07571039991719071, -0.13466460554427698], (-0.7419649188498894, 0.6226963561227075, 0.012382684241208437, 0.2481611903553049))
+        #Need to find new singlePose and hardcode it in
+        self.homePose = singlePose
+        self.retractionStagingPose = singlePose
+        self.dropOffStagingPose = singlePose
+        self.dropOffPose = singlePose
+        self.dropPose = singlePose
 
-    def execute(self, userdata):
+    def execute(self, userdata):        
         smach.loginfo(self.__class__.__name__ + " must be subclassed. It does nothing.")
         time.sleep(SLEEP_TIME)
         self.counter += 1
@@ -28,63 +36,122 @@ class StateTestClass(smach.State):
             self.outcomes = self.get_registered_outcomes()
         return self.outcomes[self.counter%len(self.outcomes)]
 
-
 class Start(StateTestClass):
-    def __init__(self):
+    def __init__(self, davinciArm):
         super(self.__class__, self).__init__()
-        smach.State.__init__(self, outcomes=['success'])
+        smach.State.__init__(self, outcomes = ['success'])
+        self.davinciArm = davinciArm
+
+    def execute(self, userdata):
+        print "Donothing"
+        return 'success'
 
 class MoveToRetractionStagingArea(StateTestClass):
-    def __init__(self):
+    def __init__(self, davinciArm):
         super(self.__class__, self).__init__()
-        smach.State.__init__(self, outcomes=['success', 'failure'])
+        smach.State.__init__(self, outcomes = ['success', 'failure'])
+        self.davinciArm = davinciArm        
+    
+    def execute(self, userdata):
+        print "Donothing"
 
 class HomePosition(StateTestClass):
-    def __init__(self):
+    def __init__(self, davinciArm):
         super(self.__class__, self).__init__()
-        smach.State.__init__(self, outcomes=['success', 'failure'])
+        smach.State.__init__(self, outcomes = ['success', 'failure'])
+        self.davinciArm = davinciArm
+    
+    def execute(self, userdata):
+        print "Donothing"
 
 class MoveToGraspPoint(StateTestClass):
-    def __init__(self):
+    def __init__(self, davinciArm):
         super(self.__class__, self).__init__()
-        smach.State.__init__(self, outcomes=['success'])
+        smach.State.__init__(self, outcomes = ['success'])
+        self.davinciArm = davinciArm
+    
+    def execute(self, userdata):
+        print "Donothing"
 
 class GraspBlock(StateTestClass):
-    def __init__(self):
+    def __init__(self, davinciArm):
         super(self.__class__, self).__init__()
-        smach.State.__init__(self, outcomes=['checkGrasp'])
+        smach.State.__init__(self, outcomes = ['success', 'failure'])
+        self.davinciArm = davinciArm
+    
+    def execute(self, userdata):
+        print "Donothing"
 
 class CheckGrasp(StateTestClass):
-    def __init__(self):
+    def __init__(self, davinciArm):
         super(self.__class__, self).__init__()
         smach.State.__init__(self, outcomes=['success', 'failure'])
+        self.davinciArm = davinciArm
+    
+    def execute(self, userdata):
+        print "Donothing"
 
 class ReleaseGrippersNoBlock(StateTestClass):
-    def __init__(self):
+    def __init__(self, davinciArm):
         super(self.__class__, self).__init__()
         smach.State.__init__(self, outcomes=['success', 'failure'])
+        self.davinciArm = davinciArm
+    
+    def execute(self, userdata):
+        print "Donothing"
 
 class ReturnToRetractionStagingAreaWithBlock(StateTestClass):
-    def __init__(self):
+    def __init__(self, davinciArm):
         super(self.__class__, self).__init__()
         smach.State.__init__(self, outcomes=['success', 'failure'])
+        self.davinciArm = davinciArm
+
+    def execute(self, userdata):
+        print "Donothing"
 
 class MoveToDropOffStagingArea(StateTestClass):
-    def __init__(self):
+    def __init__(self, davinciArm):
         super(self.__class__, self).__init__()
         smach.State.__init__(self, outcomes=['success', 'failure'])
+        self.davinciArm = davinciArm
+
+    def execute(self, userdata):
+        print "Donothing"
 
 class MoveToDropOffPoint(StateTestClass):
-    def __init__(self):
+    def __init__(self, davinciArm):
         super(self.__class__, self).__init__()
         smach.State.__init__(self, outcomes=['success', 'failure'])
+        self.davinciArm = davinciArm
+
+    def execute(self, userdata):
+        print "Donothing"
 
 class ReleaseGripper(StateTestClass):
-    def __init__(self):
+    def __init__(self, davinciArm):
         super(self.__class__, self).__init__()
         smach.State.__init__(self, outcomes=['success', 'failure'])
+        self.davinciArm = davinciArm
+
+    def execute(self, userdata):
+        print "Donothing"
 
 class CheckDropOff(StateTestClass):
-    def __init__(self):
+    def __init__(self, davinciArm):
         super(self.__class__, self).__init__()
         smach.State.__init__(self, outcomes=['success', 'failure'])
+        self.davinciArm = davinciArm
+
+    def execute(self, userdata):
+        print "Donothing"
+
+class Abort(StateTestClass):
+    def __init__(self, davinciArm):
+        super(self.__class__, self).__init__()
+        smach.State.__init__(self, outcomes=['failure'])
+        self.davinciArm = davinciArm
+        print "Do Nothing, init Abort"
+
+    def execute(self, userdata):
+        # self.davinciArm.ravenController().stop()
+        print "Do nothing"
--- src/fls_state_machine.py
+++ src/fls_state_machine.py
@@ -6,66 +6,109 @@
 import roslib
 import time
 import smach
+import rospy
 
 from base_classes import *
 
-def setup_state_machine():
-    smach.loginfo("Initializing state machine")
-    state_machine = smach.StateMachine(outcomes=['SUCCESS', 'FAILURE'])
-    with state_machine:
-        smach.StateMachine.add('START',
-            Start(),
-            transitions={'success':'MOVE_TO_RETRACTION_STAGING_AREA'})
+from davinci_utils import raven_util
+from davinci_utils import raven_constants
+from davinci_trajectory.raven_controller import RavenController
+from davinci_trajectory.raven_arm import RavenArm
 
-        smach.StateMachine.add('MOVE_TO_RETRACTION_STAGING_AREA',
-            MoveToRetractionStartingArea(),
-            transitions={'success':'MOVE_TO_GRASP_POINT', 'failure': 'HOME_POSITION'})
+class MasterClass:
+    def __init__(self):
+        rospy.init_node('Master_SM_FLS',anonymous=False)
+        self.state_machine = smach.StateMachine(outcomes=['SUCCESS', 'FAILURE'])
+        self.davinciArm = RavenArm(raven_constants.Arm.Right)
+        self.setup_state_machine()
 
-        smach.StateMachine.add('HOME_POSITION',
-            HomePosition(),
-            transitions={'success':'MOVE_TO_RETRACTION_STAGING_AREA', 'failure': 'HOME_POSITION'})
+    def setup_state_machine(self):
+        smach.loginfo("Initializing state machine")
+        rospy.sleep(1)
 
-        smach.StateMachine.add('MOVE_TO_GRASP_POINT',
-            MoveToGraspPoint(),
-            transitions={'success':'GRASP_BLOCK', 'failure': 'MOVE_TO_RETRACTION_STAGING_AREA'})
+        with self.state_machine:
+            smach.StateMachine.add('START',
+                Start(self.davinciArm),
+                transitions={'success':'MOVE_TO_RETRACTION_STAGING_AREA'})
 
-        smach.StateMachine.add('GRASP_BLOCK',
-            GraspBlock(),
-            transitions={'success':'RETURN_TO_RETRACTION_STAGING_AREA_WITH_BLOCK', 'failure': 'RELEASE_GRIPPERS_NO_BLOCK')
+            smach.StateMachine.add('MOVE_TO_RETRACTION_STAGING_AREA',
+                MoveToRetractionStagingArea(self.davinciArm),
+                transitions={'success':'MOVE_TO_GRASP_POINT', 'failure': 'HOME_POSITION'})
 
-        smach.StateMachine.add('CHECK_GRASP',
-            CheckGrasp(),
-            transitions={'success':'MOVE_TO_DROP_OFF_STAGING_AREA', 'failure': 'RELEASE_GRIPPERS_NO_BLOCK'})
+            smach.StateMachine.add('HOME_POSITION',
+                HomePosition(self.davinciArm),
+                transitions={'success':'MOVE_TO_RETRACTION_STAGING_AREA', 'failure': 'HOME_POSITION'})
 
-        smach.StateMachine.add('RELEASE_GRIPPERS_NO_BLOCK',
-            ReleaseGrippersNoBlock(),
-            transitions={'success':'MOVE_TO_RETRACTION_STAGING_AREA', 'failure': 'RELEASE_GRIPPERS_NO_BLOCK'})
+            smach.StateMachine.add('MOVE_TO_GRASP_POINT',
+                MoveToGraspPoint(self.davinciArm),
+                transitions={'success':'GRASP_BLOCK'})
 
-        smach.StateMachine.add('RETURN_TO_RETRACTION_STAGING_AREA_WITH_BLOCK',
-            ReturnToRetractionStagingAreaWithBlock(),
-            transitions={'success':'CHECK_GRASP', 'failure': 'ABORT'})
+            smach.StateMachine.add('GRASP_BLOCK',
+                GraspBlock(self.davinciArm),
+                transitions={'success':'RETURN_TO_RETRACTION_STAGING_AREA_WITH_BLOCK', 'failure': 'RELEASE_GRIPPERS_NO_BLOCK'})
 
-        smach.StateMachine.add('MOVE_TO_DROP_OFF_STAGING_AREA',
-            MoveToDropOffStagingArea(),
-            transitions={'success':'MOVE_TO_DROP_OFF_POINT', 'failure': 'ABORT'})
+            smach.StateMachine.add('CHECK_GRASP',
+                CheckGrasp(self.davinciArm),
+                transitions={'success':'MOVE_TO_DROP_OFF_STAGING_AREA', 'failure': 'RELEASE_GRIPPERS_NO_BLOCK'})
 
-        smach.StateMachine.add('MOVE_TO_DROP_OFF_POINT',
-            MoveToDropOffPoint(),
-            transitions={'success':'RELEASE_GRIPPERS', 'failure': 'ABORT'})
+            smach.StateMachine.add('RELEASE_GRIPPERS_NO_BLOCK',
+                ReleaseGrippersNoBlock(self.davinciArm),
+                transitions={'success':'MOVE_TO_RETRACTION_STAGING_AREA', 'failure': 'RELEASE_GRIPPERS_NO_BLOCK'})
 
-        smach.StateMachine.add('RELEASE_GRIPPERS',
-            ReleaseGripper(),
-            transitions={'success':'CHECK_DROP_OFF', 'failure': 'RELEASE_GRIPPERS'})
+            smach.StateMachine.add('RETURN_TO_RETRACTION_STAGING_AREA_WITH_BLOCK',
+                ReturnToRetractionStagingAreaWithBlock(self.davinciArm),
+                transitions={'success':'CHECK_GRASP', 'failure': 'ABORT'})
 
-        smach.StateMachine.add('CHECK_DROP_OFF',
-            CheckDropOff(),
-            transitions={'success':'SUCCESS', 'failure': 'ABORT'})
+            smach.StateMachine.add('MOVE_TO_DROP_OFF_STAGING_AREA',
+                MoveToDropOffStagingArea(self.davinciArm),
+                transitions={'success':'MOVE_TO_DROP_OFF_POINT', 'failure': 'ABORT'})
 
-    state_machine.execute()
+            smach.StateMachine.add('MOVE_TO_DROP_OFF_POINT',
+                MoveToDropOffPoint(self.davinciArm),
+                transitions={'success':'RELEASE_GRIPPERS', 'failure': 'ABORT'})
+
+            smach.StateMachine.add('RELEASE_GRIPPERS',
+                ReleaseGripper(self.davinciArm),
+                transitions={'success':'CHECK_DROP_OFF', 'failure': 'RELEASE_GRIPPERS'})
+
+            smach.StateMachine.add('CHECK_DROP_OFF',
+                CheckDropOff(self.davinciArm),
+                transitions={'success':'SUCCESS', 'failure': 'ABORT'})
+
+            smach.StateMachine.add('ABORT', Abort(self.davinciArm), transitions={'failure': 'FAILURE'})
+
+    def run(self):
+        self.davinciArm.start()
+        rospy.sleep(2)
+        
+        rate = rospy.Rate(1)
+        try:
+            self.state_machine.execute()
+            #outcome = self.sm.execute(userData)
+            # thread1 = threading.Thread(target=self.sm.execute,args=(userData,))
+            # thread1.daemon = True
+            # thread1.start()
+            
+            # thread2 = threading.Thread(target=self.other_sm.execute,args=(otherUserData,))
+            # thread2.daemon = True
+            # thread2.start()
+        except Exception, e:
+            print e
+
+        while not rospy.is_shutdown():
+            if not self.state_machine.is_running():
+                print "State Machine stopped"
+                rospy.sleep(0.5)
+                rospy.signal_shutdown('state machines finished')
+                break
+            rate.sleep()
+
+        self.davinciArm.stop()
 
 def main():
     smach.loginfo("Starting main...")
-    setup_state_machine()
+    master = MasterClass()
+    master.run()
 
 
 if __name__ == '__main__':
